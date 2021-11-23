da1ea99b1d25d3e56b8a22c3eac5b85f17e6ee6d
Nico Dann <nicodann@gmail.com>
2021-11-23 20:37:28 +0000

    Handle Errors

---
+++ breedFetcher.js
@@ -0,0 +1,13 @@
+const request = require('request');
+
+const Breed = process.argv.slice(2);
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${Breed}`, (error, response, body) => {
+  const data = JSON.parse(body);
+  if (data[0] !== undefined) {
+    console.log(data[0].description);
+  } else {
+    console.log(`Cannot find any breed info, please try again.`)
+  }
+  return;
+})
